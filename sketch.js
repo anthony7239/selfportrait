026bb59da7c90321651b2d40ccf8a3152e90d8a2
anthony7239 <anthony7239@gmail.com>
2017-09-05 20:29:03 -0400

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,39 @@
+function setup() {
+    createCanvas(640, 460);
+}
+
+function draw() {
+    background("gray");
+    
+    fill("darkblue");
+    ellipse(300, 470, 400, 150);
+    
+    fill("#ffdbaa");
+    ellipse(300, 250, 300, 350);
+    
+    fill("white");
+    ellipse(250, 200, 50, 50);
+    
+    fill("white");
+    ellipse(350, 200, 50, 50);
+    
+    fill("brown");
+    ellipse(250, 200, 35, 35);
+    
+    fill("brown");
+    ellipse(350, 200, 35, 35);
+    
+    fill("black");
+    ellipse(250, 200, 20, 20);
+    
+    fill("black");
+    ellipse(350, 200, 20, 20);
+    
+	
+    
+    fill("black");
+    ellipse(300, 350, 80, 20);
+    
+    stroke("black");
+    
+}
